ba21deb1b816bb1beadfb8617a5c86f199e2b0d1
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2017-12-04 22:44:17 -0200

    Aula 16 - Matchers de comparação

---
+++ rspec_tdd_1/spec/matchers/comparacao/comparacao_spec.rb
@@ -0,0 +1,23 @@
+describe 'Matchers de Comparação' do
+  it '#equal - Testa se é o mesmo objeto' do
+    x = "ruby"
+    y = "ruby"
+
+    #objetos diferentes
+    expect(x).not_to equal(y)
+  end
+
+  it '#be - Testa se é o mesmo objeto' do
+    x = "ruby"
+    y = "ruby"
+
+    expect(x).not_to be(y)
+  end
+
+  it '#eql - Testa o valor' do
+    x = "ruby"
+    y = "ruby"
+
+    expect(x).to eql(y)
+  end
+end
