31a96bd0c7770d1ee379c535bfb894f74f92824e
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2017-12-05 14:55:29 -0200

    Aula 22 - Matchers de Erros

--- rspec_tdd_1/spec/calculator/calculator_spec.rb
+++ rspec_tdd_1/spec/calculator/calculator_spec.rb
@@ -1,6 +1,14 @@
 require 'calculator'
 
 describe Calculator do
+  context '#div' do
+    it 'divide by 0' do
+      expect{subject.div(3,0)}.to raise_error(ZeroDivisionError)
+      expect{subject.div(3,0)}.to raise_error("divided by 0")
+      expect{subject.div(3,0)}.to raise_error(ZeroDivisionError,"divided by 0")
+      expect{subject.div(3,0)}.to raise_error(/divided/)
+    end
+  end
   context '#sum' do
     it 'with positive numbers' do
       result = subject.sum(5, 7)
