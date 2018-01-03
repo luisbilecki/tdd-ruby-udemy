b30a7990e3c9b2437d70ee68c7430c1ac731f513
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-01-03 19:31:17 -0200

    Aula 47 - as_null_object

--- rspec_tdd_1/spec/test_doubles/user_spec.rb
+++ rspec_tdd_1/spec/test_doubles/user_spec.rb
@@ -9,4 +9,17 @@ describe "Teste double" do
     puts user.perfil
   end
 
+  it 'as_null_object' do
+    #as_null_object se tiver algum erro no double, ele ignora.
+    user = double('User').as_null_object
+    allow(user).to receive_messages(name: 'Luís', password: 'secret')
+    puts user.name
+    puts user.password
+
+    allow(user).to receive(:perfil).and_return("Administrador")
+    puts user.perfil
+
+    user.abc
+  end
+
 end
