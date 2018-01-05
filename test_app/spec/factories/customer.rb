7c82b0310c4db638ad82f03573ab621e73045bf2
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-01-04 23:36:55 -0200

    Aula 50 - Conhecendo o FactoryBot (FactoryGirl)

---
+++ test_app/spec/factories/customer.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :customer do
+    name "Luís Felipe"
+    email "luis.bilecki@gmail.com"
+  end
+end
--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
 
   #fixtures :customers
-  fixtures :all
+  #fixtures :all
 
   it 'create a customer' do
-    customer = customers(:luis)
+    #customer = customers(:luis)
+    customer = create(:customer)
     expect(customer.full_name).to eq("Sr. Luís Felipe")
   end
 end
