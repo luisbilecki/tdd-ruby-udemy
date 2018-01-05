dbe17fc98ddee431980d51b51fafef07b3eca62e
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-01-05 09:32:57 -0200

    Aula 53 - Herança

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -12,8 +12,15 @@ RSpec.describe Customer, type: :model do
   end
 
   it '#full_name - sobreescrevendo atributos e usando aliases' do
-    customer = create(:user, name: "Luís")
+    customer = create(:user, name: "Luís", vip: false, days_to_pay: 10)
     expect(customer.full_name).to eq("Sr. Luís")
+    expect(customer.vip).to eq(false)
+  end
+
+  it '#vip - herança exemplo' do
+    customer = create(:customer_vip, name: "Luís")
+    expect(customer.full_name).to eq("Sr. Luís")
+    expect(customer.vip).to eq(true)
   end
 
   it { expect{create(:customer)}.to change{Customer.all.size}.by(1) }
