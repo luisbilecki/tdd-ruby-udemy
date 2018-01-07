59ec88e4a96cd58e5872332fe011190be038d9ef
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-01-07 21:05:34 -0200

    Aula 68 - VCR c/ metadados do RSpec

--- test_app/spec/spec_helper.rb
+++ test_app/spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'webmock/rspec'
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
+  config.configure_rspec_metadata!
 end
 
 
