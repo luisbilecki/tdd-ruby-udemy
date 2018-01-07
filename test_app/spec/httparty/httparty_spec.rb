f10ba2819c79595e20eaffa8ab4d7d366ea5b02d
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-01-07 19:07:04 -0200

    Aula 66 - Webmock

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,5 +1,8 @@
 describe 'HTTParty' do
-  it 'HTTParty' do
+  it 'content-party' do
+   stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
+    to_return(status: 200, body: "", headers: {'content-type': 'application/json'})
+
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
     content_type = response.headers['content-type']
     puts content_type
