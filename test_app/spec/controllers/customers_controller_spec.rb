daf5ef6aef6854c06113ae5ec6540578b6ef91bd
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-01-11 11:20:16 -0200

    Aula 79 - Testando controller sem autenticação)

--- test_app/spec/controllers/customers_controller_spec.rb
+++ test_app/spec/controllers/customers_controller_spec.rb
@@ -1,13 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe CustomersController, type: :controller do
-  it 'responds successfully' do
-    get :index
-    expect(response).to be_success
+  describe 'as a Guest' do
+    context '#index' do
+      it 'responds successfully' do
+        get :index
+        expect(response).to be_success
+      end
+
+      it 'responds a 200 response' do
+        get :index
+        expect(response).to have_http_status "200"
+      end
+    end
+
+    context '#show' do
+      it 'responds a 302 response - not authorized' do
+        create(:customer)
+        get :show, params: { id: Customer.all.first.id}
+        expect(response).to have_http_status "302"
+      end
+    end
   end
 
-  it 'responds a 200 response' do
-    get :index
-    expect(response).to have_http_status "200"
+  context 'as a Logged User' do
+    it 'responds a 200 response' do
+      member = create(:member)
+      customer = create(:customer)
+
+      sign_in member
+      get :show, params: { id: customer.id}
+      expect(response).to have_http_status "200"
+    end
+
+    it 'render show template' do
+      member = create(:member)
+      customer = create(:customer)
+
+      sign_in member
+      get :show, params: { id: customer.id}
+      expect(response).to render_template(:show)
+    end
   end
+
+
 end
