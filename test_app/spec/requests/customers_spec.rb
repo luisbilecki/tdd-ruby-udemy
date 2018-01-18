f77853731310a55500279bda82752320bae07bbc
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-01-18 17:00:57 -0200

    Aula 96 - Patch/Put JSON

--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -38,5 +38,22 @@ RSpec.describe "Customers", type: :request do
           email: customer_params.fetch(:email)
       )
     end
+
+    it 'update - JSON' do
+      member = create(:member)
+      login_as(member, scope: :member)
+
+      headers = {"ACCEPT" => "application/json"}
+
+      customer = Customer.first
+      customer.name += " - ATUALIZADO"
+
+      patch "/customers/#{customer.id}.json", params: { customer: customer.attributes }, headers: headers
+      expect(response.body).to include_json(
+          id: /\d/,
+          name: customer.name,
+          email: customer.email
+      )
+    end
   end
 end
