8b46b8b6a7d4b0d561ae3ca7aa456c01bda2c2d9
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-02-05 10:09:35 -0200

    Aula 103 - Projetando a App

--- tdd_app/spec/features/welcomes_spec.rb
+++ tdd_app/spec/features/welcomes_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
-feature "Welcome", type: :feature do
-  scenario  'Mostra a mensagem de bem-vindo' do
-    visit('/')
+feature 'Welcome', type: :feature do
+  scenario 'Mostra a mensagem de bem-vindo' do
+    visit(root_path)
     expect(page).to have_content('Bem-Vindo')
+  end
+
+  scenario 'Verificar o link de cadastro de clientes' do
+    visit(root_path)
+    expect(find('ul li')).to have_link('Cadastro de Clientes')
   end
 end
