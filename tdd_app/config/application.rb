12961bae2c6beb912087696f2eeffc74518cd4a0
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-02-13 22:03:58 -0200

    Aula 105 - Cadastro de Cliente

--- tdd_app/config/application.rb
+++ tdd_app/config/application.rb
@@ -25,6 +25,8 @@ module TddApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.i18n.default_locale = :'pt-BR'
+
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.generators do |g|
